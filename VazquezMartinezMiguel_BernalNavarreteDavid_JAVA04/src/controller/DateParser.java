54a066850f72c2ebae30d7b45fbb6ca0e7f670d3
Fallest <56403805+Fallest@users.noreply.github.com>
2021-10-27 21:08:06 +0200

    Commit
    Changes:
    *CheckID method removed from MyExceptions.
    *Some more adjustments made in MyExceptions.
    *Applied same formatting for commenting sections of the code.
    
    D

--- VazquezMartinezMiguel_BernalNavarreteDavid_JAVA04/src/controller/DateParser.java
+++ VazquezMartinezMiguel_BernalNavarreteDavid_JAVA04/src/controller/DateParser.java
@@ -8,15 +8,16 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 /**
- * 
+ *
  * @author Miguel Maria Vazquez Martinez
  * @author David Bernal Navarrete
- * 
- * This class is used to turn String objects to GregorianCalendar objects and vice versa.
- * It also has the tools required to check if a certain amount of time has passed
- * between two dates.
+ *
+ * This class is used to turn String objects to GregorianCalendar objects and
+ * vice versa. It also has the tools required to check if a certain amount of
+ * time has passed between two dates.
  */
 public class DateParser {
+
     /**
      * A SimpleDateFormat will be used to format Strings.
      */
@@ -25,11 +26,12 @@ public class DateParser {
     /*------------------------------------------------------------------------*/
     // Converts from a String to a GregorianCalendar.
     public static GregorianCalendar parseDate(String date) {
-        /** Strig parameter must have the following format: DD/MM/YYYY
-         * We will use the following constructor from GregorianCalendar
-         * GregorianCalendar(int year, int month, int day)
-         * To extract the day, month, ad year from the String, we will use the
-         * substring() method, and then convert that substring to an int.
+        /**
+         * Strig parameter must have the following format: DD/MM/YYYY We will
+         * use the following constructor from GregorianCalendar
+         * GregorianCalendar(int year, int month, int day) To extract the day,
+         * month, ad year from the String, we will use the substring() method,
+         * and then convert that substring to an int.
          */
         GregorianCalendar cal = null;
 
@@ -51,6 +53,7 @@ public class DateParser {
 
         return cal;
     }
+
     /*------------------------------------------------------------------------*/
     // Converts from GregorianCalendar to String.
     public static String parseDate(GregorianCalendar cal) {
@@ -62,8 +65,9 @@ public class DateParser {
 
     public static String toString(GregorianCalendar cal) {
         /**
-         * Creates a Date object with the same values as the GregorianCalendar parameter.
-         * Then, it converts it to a formatted string with SimpleDateFormat and the format() method.
+         * Creates a Date object with the same values as the GregorianCalendar
+         * parameter. Then, it converts it to a formatted string with
+         * SimpleDateFormat and the format() method.
          */
         try {
             Date thisDate = cal.getTime();
@@ -74,61 +78,62 @@ public class DateParser {
             return null;
         }
     }
+
     /*------------------------------------------------------------------------*/
     public static boolean passedMonth(GregorianCalendar entryDate) {
         /**
-         * Checks whether or not a month has passed between the given date
-         * and the current date.
+         * Checks whether or not a month has passed between the given date and
+         * the current date.
          */
         GregorianCalendar now = new GregorianCalendar();
-        
+
         int actualYear = now.get(Calendar.YEAR),
                 entryYear = now.get(Calendar.YEAR);
-        int actualMonth = now.get(Calendar.MONTH), 
+        int actualMonth = now.get(Calendar.MONTH),
                 entryMonth = entryDate.get(Calendar.MONTH);
-        int actualDay = now.get(Calendar.DAY_OF_MONTH), 
+        int actualDay = now.get(Calendar.DAY_OF_MONTH),
                 entryDay = entryDate.get(Calendar.DAY_OF_MONTH);
-        
-        return (((actualMonth + (11 * (actualYear - entryYear))) - entryMonth) >= 1 && 
-                (actualDay >= entryDay));
+
+        return (((actualMonth + (11 * (actualYear - entryYear))) - entryMonth) >= 1
+                && (actualDay >= entryDay));
     }
 
     /*------------------------------------------------------------------------*/
     public static boolean passedQuarter(GregorianCalendar entryDate) {
         /**
-         * Checks whether or not 4 months have passed between the given date
-         * and the current date.
+         * Checks whether or not 4 months have passed between the given date and
+         * the current date.
          */
         GregorianCalendar now = new GregorianCalendar();
-        
+
         int actualYear = now.get(Calendar.YEAR),
                 entryYear = now.get(Calendar.YEAR);
-        int actualMonth = now.get(Calendar.MONTH), 
+        int actualMonth = now.get(Calendar.MONTH),
                 entryMonth = entryDate.get(Calendar.MONTH);
-        int actualDay = now.get(Calendar.DAY_OF_MONTH), 
+        int actualDay = now.get(Calendar.DAY_OF_MONTH),
                 entryDay = entryDate.get(Calendar.DAY_OF_MONTH);
-        
-        return (((actualMonth + (11 * (actualYear - entryYear))) - entryMonth) % 4 == 0 &&
-                (actualDay >= entryDay));
+
+        return (((actualMonth + (11 * (actualYear - entryYear))) - entryMonth) % 4 == 0
+                && (actualDay >= entryDay));
     }
-    
+
     /*------------------------------------------------------------------------*/
     public static boolean passedYear(GregorianCalendar entryDate) {
         /**
-         * Checks whether or not a year has passed between the given date
-         * and the current date.
+         * Checks whether or not a year has passed between the given date and
+         * the current date.
          */
         GregorianCalendar now = new GregorianCalendar();
-        
+
         int actualYear = now.get(Calendar.YEAR),
                 entryYear = now.get(Calendar.YEAR);
-        int actualMonth = now.get(Calendar.MONTH), 
+        int actualMonth = now.get(Calendar.MONTH),
                 entryMonth = entryDate.get(Calendar.MONTH);
-        int actualDay = now.get(Calendar.DAY_OF_MONTH), 
+        int actualDay = now.get(Calendar.DAY_OF_MONTH),
                 entryDay = entryDate.get(Calendar.DAY_OF_MONTH);
-        
-        return ((actualYear > entryYear) &&
-                ((actualMonth + (11 * (actualYear - entryYear))) >= entryMonth) &&
-                (actualDay >= entryDay));
+
+        return ((actualYear > entryYear)
+                && ((actualMonth + (11 * (actualYear - entryYear))) >= entryMonth)
+                && (actualDay >= entryDay));
     }
 }
--- VazquezMartinezMiguel_BernalNavarreteDavid_JAVA04/src/model/Analyst.java
+++ VazquezMartinezMiguel_BernalNavarreteDavid_JAVA04/src/model/Analyst.java
@@ -5,81 +5,72 @@ import controller.DateParser;
 /**
  * @author Miguel Maria Vazquez Martinez
  * @author David Bernal Navarrete
- * 
- * Class Analyst:
- * Inherits from Employee and implements DateInterface
- * This class adds 2 other attributes:
- *      -static float annualPlus: An annual salary plus for every Analyst.
- *          It must be between 0 and 1, since it is a %.
- *      -String projectName: The project that each Analyst is assigned to work 
- *          on.
- * 
- * This class must implement all abstract methods from both Employee and 
+ *
+ * Class Analyst: Inherits from Employee and implements DateInterface This class
+ * adds 2 other attributes: -static float annualPlus: An annual salary plus for
+ * every Analyst. It must be between 0 and 1, since it is a %. -String
+ * projectName: The project that each Analyst is assigned to work on.
+ *
+ * This class must implement all abstract methods from both Employee and
  * DateInterface.
  */
-
-public class Analyst extends Employee implements DateInterface{
+public class Analyst extends Employee implements DateInterface {
 
     // Attributes
-    
     private static float annualPlus;
     private String projectName;
-    
+
     /*------------------------------------------------------------------------*/
     // Constructors
-    
-    public Analyst(String id, String name, String salary, String maxSalary, String entryDate){
+    public Analyst(String id, String name, String salary, String maxSalary, String entryDate) {
         super(id, name, salary, maxSalary, entryDate);
     }
-    
+
     public Analyst(String id, String name, String salary, String maxSalary,
-            String entryDate, String annualPlus, String projectName){
+            String entryDate, String annualPlus, String projectName) {
         super(id, name, salary, maxSalary, entryDate);
         setAnnualPlus(annualPlus);
         setProjectName(projectName);
     }
-    
+
     /*------------------------------------------------------------------------*/
     // Setters
-    
     public final void setAnnualPlus(String annualPlus) {
         Analyst.annualPlus = Float.parseFloat(annualPlus);
     }
-    
+
     public final void setProjectName(String projectName) {
         this.projectName = projectName;
     }
-    
+
     /*------------------------------------------------------------------------*/
     // Getters
-    
     public static float getAnnualPlus() {
         return Analyst.annualPlus;
     }
-    
+
     public String getProjectName() {
         return this.projectName;
     }
-    
+
     /*------------------------------------------------------------------------*/
     // Inherited abstract methods from Employee
-    
     @Override
     public String toString() {
-        return "Analyst;" + this.name + ";" + this.id + ";" + this.salary + ";" 
-                + Analyst.maxSalary + ";"  + this.entryDate + ";"  
+        return "Analyst;" + this.name + ";" + this.id + ";" + this.salary + ";"
+                + Analyst.maxSalary + ";" + this.entryDate + ";"
                 + Analyst.annualPlus + ";" + this.projectName;
     }
-    
+
     @Override
-    public void updateSalary(){
-        if (passedOneYear())
+    public void updateSalary() {
+        if (passedOneYear()) {
             this.salary += (this.salary * Analyst.annualPlus);
+        }
     }
-    
+
     /*------------------------------------------------------------------------*/
     // Inherited abstract methods from DateInterface
-    
     @Override
     public boolean passedOneMonth() {
         return DateParser.passedMonth(this.entryDate);
