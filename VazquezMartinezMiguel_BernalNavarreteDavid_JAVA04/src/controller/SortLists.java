006e19af61a535b0cc0a76375d866d0a6cd20485
Miguel M. Vázquez <77959397+DevLawyer@users.noreply.github.com>
2021-10-27 13:11:22 +0200

    SorList class
    Contains methods for copy and sort both list.

---
+++ VazquezMartinezMiguel_BernalNavarreteDavid_JAVA04/src/controller/SortLists.java
@@ -0,0 +1,34 @@
+package controller;
+
+import view.FrameMain;
+
+/**
+ *
+ * @author migva
+ */
+public class SortLists {
+    
+    public void copyToCollection(){
+        
+    }
+    
+    public void sortOwnList(){
+        
+    }
+    
+    public void sortCollection(){
+        
+    }
+    
+    public static int getOwnListTime(){
+        return ownListTime;
+    }
+    
+    public static int getCollectionTime(){
+        return collectionTime;
+    }
+    
+    private static int ownListTime;
+    private static int collectionTime;
+    private List myList = FrameMain.getMainList();
+}
